f97dbbf19ee4396c60c2c40a42195a07741e2540
Francis Borja <fborja4400@gmail.com>
2021-11-16 13:19:39 -0500

    Tutorial: Pages and Title Button

--- Pet Life/Assets/Scripts/TutorialManager.cs
+++ Pet Life/Assets/Scripts/TutorialManager.cs
@@ -9,30 +9,50 @@ public class TutorialManager : MonoBehaviour
 {
     public int page = 1;
     public TextMeshProUGUI pageText;
+    public GameObject[] pages; // array of page game objects
     public Button nextButton;
     public Button prevButton;
+    public Button titleButton;
 
     // Start is called before the first frame update
     void Start()
     {
- 
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Hide previous button if on first page
+
     }
 
     // Function to change page elements
     public void ChangePage(int newPage) {
+        // Hide current page
+        pages[page-1].gameObject.SetActive(false);
+
+        // Change page text
         page = newPage;
         pageText.text = "Page " + page;
 
+        // Show new page
+        pages[page-1].gameObject.SetActive(true);
+
+        // Show/hide page buttons
         if (page <= 1) {
             prevButton.gameObject.SetActive(false);
         } else {
             prevButton.gameObject.SetActive(true);
         }
+        if (page == pages.Length) {
+            nextButton.gameObject.SetActive(false);
+        } else {
+            nextButton.gameObject.SetActive(true);
+        }
+    }
+
+    // Changes scene to title screen
+    public void ToTitle() {
+        SceneManager.LoadScene("Title Scene");
     }
 }

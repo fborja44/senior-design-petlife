78e8b89d15842138f6d25633f8a191a775b4abb8
Francis Borja <fborja4400@gmail.com>
2021-11-18 11:15:27 -0500

    Update pet selection prefabs

--- Pet Life/Assets/Scripts/GameManager.cs
+++ Pet Life/Assets/Scripts/GameManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject PlayerPet;
+    public GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
--- Pet Life/Assets/Scripts/SelectPet.cs
+++ Pet Life/Assets/Scripts/SelectPet.cs
@@ -6,7 +6,9 @@ using UnityEngine.EventSystems;
 
 public class SelectPet : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    private GameManager gameManager;
     public GameObject pet;
+    public int petIndex;
     public Image selection;
     public Sprite disc;
     public Sprite disc_alt;
@@ -14,7 +16,8 @@ public class SelectPet : MonoBehaviour, IPointerEnterHandler, IPointerExitHandle
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Initialize vars
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -24,10 +27,23 @@ public class SelectPet : MonoBehaviour, IPointerEnterHandler, IPointerExitHandle
     }
 
     public void OnPointerEnter(PointerEventData eventData) {
+        // Highlight selection
         selection.GetComponent<Image>().sprite = disc_alt;
     }
 
     public void OnPointerExit(PointerEventData eventData) {
+        // Remove highlight
         selection.GetComponent<Image>().sprite = disc;
+    }
+
+    public void OnMouseDown() {
+        // Update the player's pet sprite
+        Debug.Log("Clicked " + petIndex);
+
+        // Set Pet GameObject in GameManager
+        // TODO
+
+        // Change scene to gameplay
+        // TODO
     }
 }
